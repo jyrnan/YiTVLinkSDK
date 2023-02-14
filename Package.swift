d43bd29cd14c648a504316a2d4f308d232875dbb
jyrnan <yoeking@gmail.com>
2023-02-14 11:22:10 +0800

    Add version info

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "YiTVLinkSDK",
+    platforms: [ // 本 Package 适用的平台
+        .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
