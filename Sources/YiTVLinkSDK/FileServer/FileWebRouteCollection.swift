b8d6ed9c308c8a9ef2fca24ca7fd66e444cd401f
jyrnan <yoeking@gmail.com>
2023-05-05 09:30:08 +0800

    remove Range parameter from request headers for right respond

--- Sources/YiTVLinkSDK/FileServer/FileWebRouteCollection.swift
+++ Sources/YiTVLinkSDK/FileServer/FileWebRouteCollection.swift
@@ -24,18 +24,25 @@ struct FileWebRouteCollection: RouteCollection {
       throw Abort(.badRequest)
     }
     
-    print("Req Decription:\n----\n",req.description, "\n")
-    print("Req headers:\n----\n", req.headers, "\n")
+    /// 下载中心的下载请求头里面有这个选项，需要去除掉
+    ///  否则会回复400错误，莫非Vipor不支持Range参数？
+    req.headers.remove(name: "Range")
     
+    print("\nReq Decription:\n----\n",req.description, "\n----\n")
+    print("\nReq headers:\n----\n", req.headers, "\n----\n")
+    
+    /// 获取共享文件的本地路径
     guard let fileURL = server?.sharingFileURLs[filename] else {return Response(status: .notFound)}
-    print(fileURL)
-    let response = req.fileio.streamFile(at: fileURL.path)
-    print(response.headers["Content-Length"])
-    let length = response.headers["Content-Length"]
-    response.headers.add(name: "Content-Length", value: length.first!)
-    response.headers.add(name: "Content-Encoding", value: "identity")
     
-    print("Response headers:\n----\n", response.headers, "\n")
+    /// 返回数据
+    let response = req.fileio.streamFile(at: fileURL.path)
+//    print(response.headers["Content-Length"])
+//    let length = response.headers["Content-Length"]
+//    response.headers.add(name: "Content-Length", value: length.first!)
+    
+    print("\nResponse description:\n----\n", response.description, "\n----\n")
+    print("\nResponse headers:\n----\n", response.headers, "\n----\n")
+    
     return response
   }
 }
