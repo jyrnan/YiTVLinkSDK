79e28ed8c0e11dcbb817fd7d8ce4440f0ee2a43e
jyrnan <yoeking@gmail.com>
2023-05-06 17:45:43 +0800

    work around the bytes=0-0 resopnse is bad request

--- Sources/YiTVLinkSDK/FileServer/FileWebRouteCollection.swift
+++ Sources/YiTVLinkSDK/FileServer/FileWebRouteCollection.swift
@@ -25,21 +25,22 @@ struct FileWebRouteCollection: RouteCollection {
     }
     
     /// 下载中心的下载请求头里面有这个选项，需要去除掉
-    ///  否则会回复400错误，莫非Vipor不支持Range参数？
-    if filename.hasSuffix("apk") {
-      req.headers.remove(name: "Range")}
+    ///  否则会回复400错误，Vipor不支持Range bytes=0-0参数,
+    ///  解决方法是将request headers里面的bytes=0-0 改成bytes=0-1
+    if req.headers.contains(where: { name, value in
+      name == "Range" && value == "bytes=0-0"
+    }) {
+      req.headers.replaceOrAdd(name: "Range", value: "bytes=0-1")
+    }
     
     print("\nReq Decription:\n----\n",req.description, "\n----\n")
     print("\nReq headers:\n----\n", req.headers, "\n----\n")
     
     /// 获取共享文件的本地路径
     guard let fileURL = server?.sharingFileURLs[filename] else {return Response(status: .notFound)}
-    
+        
     /// 返回数据
     let response = req.fileio.streamFile(at: fileURL.path)
-//    print(response.headers["Content-Length"])
-//    let length = response.headers["Content-Length"]
-//    response.headers.add(name: "Content-Length", value: length.first!)
     
     print("\nResponse description:\n----\n", response.description, "\n----\n")
     print("\nResponse headers:\n----\n", response.headers, "\n----\n")
